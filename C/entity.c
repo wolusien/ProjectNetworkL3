a463df33a81542fbeda9fcfbf39276ec0c886ac0
wolusien <djebali.wissam@gmail.com>
2016-04-04 01:24:19 +0200

    Insertion d'entité en cours de test

--- C/entity.c
+++ C/entity.c
@@ -1,54 +1,33 @@
 #include "entity.h"
 
-/*
-Function taking ip address of form "127.0.0.1" and change it to "127.000.000.001"
-*/
-char* ip_addZero(char* ip){
-  int i;
-  if(strlen(ip)==9){
-    if(ip[3]=='.' && '.'==ip[5] && '.'==ip[7]){
-      char* ip_final = malloc(sizeof(char)*15);
-      for (i = 0; i < 4; i++) {
-        ip_final[i] = ip[i];
-      }
-      ip_final[4]='0';
-      ip_final[5]='0';
-      ip_final[6]=ip[4];
-      ip_final[7]=ip[5];
-      ip_final[8]='0';
-      ip_final[9]='0';
-      ip_final[10]=ip[6];
-      ip_final[11]=ip[7];
-      ip_final[12]='0';
-      ip_final[13]='0';
-      ip_final[14]=ip[8];
-      return ip_final;
-    }
-  }
-  return NULL;
-}
 
-
-/*
-  Function that create a struct entity containing information about an entity wich will be use for insertion or other
+/*!Function which initialize an entity
+ *\param char id[8] id of the entity
+ *\param int my_uport udp port of the entity
+ *\param int tcp_port  tcp port of the enity
+ *\param char* ip_ad ip address of the entity
+ *\param int next_uport udp por of the next entity
+ *\param char* ipv4_ad ipv4 adress of broadcast
+ *\param int cast_port tcp port of broadcast
+ *\return entity* pointer to an entity created
  */
-entity* init_entity(char id[8], int prevudp_rport, int tcp_port, 
-                    char* ip_ad, int nextudp_sport, char* ipv4_ad, int cast_port){
+entity* init_entity(char id[8], int my_uport, int tcp_port, 
+                    char* ip_ad, int next_uport, char* ipv4_ad, int cast_port){
   if(id!=NULL && ip_ad!=NULL 
 && strlen(ip_ad)==15 
 && ipv4_ad!=NULL){
-    if(prevudp_rport>9999 && prevudp_rport<65535 
+    if(my_uport>9999 && my_uport<65535 
 && tcp_port>9999 tcp_port<65535
-&& nextudp_sport>9999 && nextudp_sport<65535
+&& next_uport>9999 && next_uport<65535
 && cast_port>9999 && cast_port<65535){
-      if(cast_port!=nextudp_sport && cast_port!=prevudp_rport && cast_port!= tcp_port 
-&& prevudp_rport != nextudp_sport && prevudp_rport!=tcp_port && nextudp_sport != tcp_port ){
+      if(cast_port!=next_uport && cast_port!=my_uport && cast_port!= tcp_port 
+&& my_uport != next_uport && my_uport!=tcp_port && next_uport != tcp_port ){
         entity* ent = malloc(sizeof(entity));
         (*ent).id = id;
-        (*ent).prevudp_rport = prevudp_rport;
+        (*ent).my_uport = my_uport;
         (*ent).tcp_port = tcp_port;
         (*ent).ip_ad = ip_ad;
-        (*ent).nextudp_sport = nextudp_sport;
+        (*ent).next_uport = next_uport;
         (*ent).ipv4_ad = ipv4_ad;
         (*ent).cast_port = cast_port;
         return ent;
--- C/entity.h
+++ C/entity.h
@@ -1,25 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-
+#include "tools.h"
 
 typedef struct entity{
   char id[8];
-  int prevudp_rport;
+  char* my_ip;
+  int my_uport;
   int tcp_port;
-  char* ip_ad;
-  int nextudp_sport;
-  char* ipv4_ad;
+  int prev_uport;
+  char* next_ip;
+  int next_uport;
+  char* cast_ip;
   int cast_port;
 }entity;
 
-entity* init_entity(char id[8], int prevudp_lport, int tcp_port, 
-                    char* ip_ad, int nextudp_lport, char* ipv4_ad, int cast_port);
+/*!Function which initialize an entity
+ *\param char id[8] id of the entity
+ *\param int my_uport udp port of the entity
+ *\param int tcp_port  tcp port of the enity
+ *\param char* ip_ad ip address of the entity
+ *\param int next_uport udp por of the next entity
+ *\param char* ipv4_ad ipv4 adress of broadcast
+ *\param int cast_port tcp port of broadcast
+ *\return entity* pointer to an entity created
+ */
+entity* init_entity(char id[8], int my_uport, int tcp_port, 
+                    char* ip_ad, int next_uport, char* ipv4_ad, int cast_port);
 
-char* ip_addZero(char* ip);
+
+int insert_entity(entity* e, char* host, int tcp_e1);

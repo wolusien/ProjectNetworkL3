3171a7ecf662d13db6270a5f9152f6c7c6c340e5
CHEANG eric <cheang@informatique.univ-paris-diderot.fr>
2016-04-10 17:59:53 +0200

    multidiffusion

--- Javaeymerivk/Multidiffusion.java
+++ Javaeymerivk/Multidiffusion.java
@@ -1,20 +1,73 @@
-import java.util.ArrayList;
+import java.net.*;
+//import java.util.ArrayList;
 
 
 public class Multidiffusion implements Runnable{
 	int port;
-	ArrayList<Entity> listEnt;
-	
-	public Multidiffusion(int port){
+	String ip;
+
+	public Multidiffusion(int port, String ip){
 		this.port=port;
-		listEnt= new ArrayList<Entity>();
+		this.ip=ip;
+		//listEnt= new ArrayList<Entity>();
 	}
 	
-	public void addEntity(Entity ent){
-		listEnt.add(ent);
+	public Multidiffusion(){
+		
+	}
+	
+	public void setIp(String ip){
+		this.ip=ip;
+	}
+	
+	public int portlibre(){
+		MulticastSocket mso=null;
+		this.port=1024;
+		while(true){
+			try{
+				mso=new MulticastSocket(port);
+				mso.close();
+				break;
+			}
+			catch(Exception e){
+				port++;
+				if(port==9999)break;
+			}
+		}
+		return port;
 	}
 
 	public void run() {
-		
+		try{
+			MulticastSocket mso=new MulticastSocket(port);
+			mso.joinGroup(InetAddress.getByName(ip));
+			byte[]data=new byte[512];
+			DatagramPacket paquet=new DatagramPacket(data,data.length);
+			while(true){
+				mso.receive(paquet);
+				String st=new String(paquet.getData(),0,paquet.getLength());
+				System.out.println("message recu : "+st);
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	//envoie multicast
+	public void down(){
+		try{
+			DatagramSocket dso=new DatagramSocket();
+			byte[]data = new byte[255];
+			String s= "DOWN";
+			data=s.getBytes();
+			InetSocketAddress ia=new InetSocketAddress(ip,port);
+			DatagramPacket paquet=new DatagramPacket(data,data.length,ia);
+			dso.send(paquet);
+			dso.close();
+
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+
 	}
 }

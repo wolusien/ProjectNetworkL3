29165706e9f8ba0a5ce0e09c7b6687040a6441ee
wolusien <djebali.wissam@gmail.com>
2016-05-03 23:43:36 +0200

    Doit tester le port multi cast sinon c'est bon

--- C/ent1.c
+++ C/ent1.c
@@ -3,19 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-  /*
-  entity* ent = malloc(sizeof(entity));
-  int r2 = init_entity(ent, "localhost");
-  if (r2==0) {
-    (*ent).my_ip = "localhost";
-    (*ent).tcp_port = 50001;
-    pthread_t th2;
-    pthread_create(&th2,NULL,pth_insertion,ent);
-    pthread_join(th2,NULL);
-  }else{
-    printf("Problem with init entity\n");
-  }
-  */
   uEntity* u= malloc(sizeof(uEntity));
   int r = init_uEntity(u);
   if (r==0) {
@@ -24,7 +11,6 @@ int main(int argc, char *argv[])
     (*u).id_app = "12345679";
     (*u).ent->next_uport1 = 1025;
     pthread_t th2;
-    //pthread_t th3;
     pthread_create(&th2,NULL,rec_udp,u);
     pthread_join(th2,NULL);
   }else{

ae7789866c5867b29c0b24f3ce0d6dab4f26e9f4
CHEANG eric <cheang@informatique.univ-paris-diderot.fr>
2016-05-12 17:18:40 +0200

    Correction

--- Java/Main.java
+++ Java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args){
 		Entity ent= new Entity();
-		ent.Init();
+		ent.Init("TRANS###");
 		/*while(true){
 			try{
 				Thread.sleep(2000);

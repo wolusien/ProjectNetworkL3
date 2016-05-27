8028c08220c01e2fd6e0d47e9e0f7652a8db9403
CHEANG eric <cheang@informatique.univ-paris-diderot.fr>
2016-05-27 13:08:18 +0200

    changement main insert

--- Java/MainInsert.java
+++ Java/MainInsert.java
@@ -1,13 +1,23 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 
 public class MainInsert {
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Quel est ip ? ");
+		String ip=sc.nextLine();
+		System.out.println("Quel est le port ? ");
+		String port=sc.nextLine();
+		int por=Integer.parseInt(port);
+		System.out.println("Id APPL ? ");
+		String appl=sc.nextLine();
+		sc.close();
 		Entity ent= new Entity();
-		ent.insertion("172.28.47.58", 1024,"TRANS###");
+		ent.insertion(ip, por,appl);
 		/*try{
 			DatagramSocket dso=new DatagramSocket();
 			byte[]data = new byte[512];
